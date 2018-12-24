6970939a17cc4a50a5e159d9df3624340afccd98
bin7096 <bin_id@163.com>
2018-12-24 22:31:45 +0800

    2018-12-24

--- progressBar.js
+++ progressBar.js
@@ -23,21 +23,47 @@ var throwError = {
 }
 
 var progressBar = {
-	width :null,
-	height:null,
+	width       :null,
+	height      :null,
+	center      :null,
+	excricle    :null,
+	incricle    :null,
+	canvas_id   :null,
+	canvas_width:null,
 	type  :[
 		'pureColorAnnular'
 	],
-	init: function(type, percent){
+	init: function(type, width, percent, canvas_id){
 		//检测类型
 		if (this.type.indexOf(type) == -1) {
 			throwError.init("type", "进度条类型不存在");return;
 		}
+		//检测宽度百分比
+		if (Math.abs(parseInt(width)) != width) {
+			throwError.init("width", "进度条宽度百分比必须为正整数");return;
+		}
 		//检测传入百分比
 		if (Math.abs(parseInt(percent)) != percent) {
-			throwError.init("percent", "进度条宽度百分比必须为正整数");return;
+			throwError.init("percent", "进度条百分比必须为正整数");return;
 		}
+		var bodyWidth = utils.getWindowWidth();
+		this.center   = Math.floor(bodyWidth / 2);
+		this.cricleStyle(width, percent);
+	},
+	canvasStyle: function(bodyWidth){
+		var obj = document.getElementById(this.canvas_id);
+		obj.style.width = this.canvas_width = bodyWidth;return;
+	},
+	cricleStyle: function(width, percent){
+		this.excricle = Math.floor(this.canvas_width * (width / 100) / 2);
+		this.incricle = Math.floor(this.excricle * 0.3);
+		console.log(this.excricle);
+		console.log(this.incricle);
+		console.log(this.center);
+	},
+	start: function(){
+
 	}
 }
 
-progressBar.init('pureColorAnnular', -30);
+progressBar.init('pureColorAnnular', 30, 20, 'progressBar');
