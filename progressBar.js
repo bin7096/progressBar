4d3f95b1793a1f8873cc06dfb1ea21d4b09516cb
bin7096 <bin_id@163.com>
2018-12-26 17:44:00 +0800

    2018-12-26

--- progressBar.js
+++ progressBar.js
@@ -23,17 +23,23 @@ var throwError = {
 }
 
 var progressBar = {
+	pi 			:Math.PI / 180,
+	obj         :null,
+	ctx         :null,
 	width       :null,
 	height      :null,
 	center      :null,
 	excricle    :null,
 	incricle    :null,
+	bgcolor		:null,
+	barcolor	:null,
 	canvas_id   :null,
 	canvas_width:null,
+	timeOut_id  :null,
 	type  :[
 		'pureColorAnnular'
 	],
-	init: function(type, width, percent, canvas_id){
+	init: function(type, width, percent, bgcolor, barcolor, canvas_id){
 		//检测类型
 		if (this.type.indexOf(type) == -1) {
 			throwError.init("type", "进度条类型不存在");return;
@@ -46,24 +52,74 @@ var progressBar = {
 		if (Math.abs(parseInt(percent)) != percent) {
 			throwError.init("percent", "进度条百分比必须为正整数");return;
 		}
-		var bodyWidth = utils.getWindowWidth();
-		this.center   = Math.floor(bodyWidth / 2);
-		this.cricleStyle(width, percent);
+		var bodyWidth  = utils.getWindowWidth();
+		this.canvas_id = canvas_id;
+		this.bgcolor   = bgcolor;
+		this.barcolor  = barcolor;
+		this.obj = this.canvasStyle(bodyWidth);
+		switch(type){
+			case "pureColorAnnular":
+				this.cricleStyle(width, percent);
+				break;
+		}
 	},
 	canvasStyle: function(bodyWidth){
 		var obj = document.getElementById(this.canvas_id);
-		obj.style.width = this.canvas_width = bodyWidth;return;
+		obj.width = this.canvas_width = bodyWidth;
+		return obj;
 	},
 	cricleStyle: function(width, percent){
 		this.excricle = Math.floor(this.canvas_width * (width / 100) / 2);
-		this.incricle = Math.floor(this.excricle * 0.3);
-		console.log(this.excricle);
-		console.log(this.incricle);
-		console.log(this.center);
+		this.incricle = Math.floor(this.excricle * 0.9);
+		this.center   = this.excricle;
+		this.obj.width  = this.obj.height = this.excricle * 2;
+		this.annularStart(percent);
 	},
-	start: function(){
+	annularStart: function(percent){
 
+		var radiu = Math.ceil(360 * percent / 100 / 20);
+
+		this.ctx = this.obj.getContext("2d");
+		
+		this.ctx.translate(0.5, 0.5);
+		this.ctx.beginPath();
+		this.ctx.fillStyle = '#CCC';
+		this.ctx.arc(this.center, this.center, this.excricle, 0, this.pi * 360, true);
+		this.ctx.fill();
+
+		var countByPB = 0;
+		this.timeOut_id = setInterval(function() {
+			progressBar.pureCricle(radiu, countByPB);
+			countByPB ++;
+		}, 100);
+	},
+	pureCricle: function(radiu, countByPB){
+		if (countByPB >= 20) {
+			clearInterval(this.timeOut_id);return;
+		}
+
+		this.ctx.fillStyle = this.barcolor;
+	},
+	annularXY: function(percent){
+		var num = Math.floor(percent / 25);
+		var angle = percent % 25;
+		switch (num) {
+			case 0:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+		}
 	}
 }
 
-progressBar.init('pureColorAnnular', 30, 20, 'progressBar');
+progressBar.init('pureColorAnnular', 30, 60, 'rgb(255, 255, 255)', '#3366FF', 'progressBar');
+
+window.onresize = function(){
+	progressBar.init('pureColorAnnular', 30, 60, 'rgb(255, 255, 255)', '#3366FF', 'progressBar');
+}
