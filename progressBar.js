8a1075d4e0e04335bc299e687e50bd095d8c15f0
bin7096 <bin_id@163.com>
2019-01-02 18:14:10 +0800

    2019-01-02

--- progressBar.js
+++ progressBar.js
@@ -1,3 +1,4 @@
+var resize_num = 0;
 var utils = {
 	getWindowSize: function() {
         return {
@@ -39,7 +40,7 @@ var progressBar = {
 	type  :[
 		'pureColorAnnular'
 	],
-	init: function(type, width, percent, bgcolor, barcolor, canvas_id){
+	init: function(type, width, percent, bgcolor, barcolor, canvas_id, num){
 		//检测类型
 		if (this.type.indexOf(type) == -1) {
 			throwError.init("type", "进度条类型不存在");return;
@@ -59,7 +60,7 @@ var progressBar = {
 		this.obj = this.canvasStyle(bodyWidth);
 		switch(type){
 			case "pureColorAnnular":
-				this.cricleStyle(width, percent);
+				this.cricleStyle(width, percent, num);
 				break;
 		}
 	},
@@ -68,48 +69,46 @@ var progressBar = {
 		obj.width = this.canvas_width = bodyWidth;
 		return obj;
 	},
-	cricleStyle: function(width, percent){
+	cricleStyle: function(width, percent, num){
 		this.excricle = Math.floor(this.canvas_width * (width / 100) / 2);
 		this.incricle = Math.floor(this.excricle * 0.9);
 		this.center   = this.excricle;
 		this.obj.width  = this.obj.height = this.excricle * 2;
-		this.annularStart(percent);
+		this.annularStart(percent, num);
 	},
-	annularStart: function(percent){
-
+	annularStart: function(percent, num){
 		this.ctx = this.obj.getContext("2d");
 		
 		this.ctx.translate(0.5, 0.5);  //解决canvas线条模糊问题
-		// this.ctx.beginPath();
-		// this.ctx.fillStyle = '#CCC';
-		// this.ctx.arc(this.center, this.center, this.excricle, 0, this.pi * 360, true);
-		// this.ctx.fill();
+		this.ctx.beginPath();
+		this.ctx.fillStyle = '#000000';
+		this.ctx.arc(this.center, this.center, this.excricle, 0, this.pi * 360, true);
+		this.ctx.fill();
 
 		var countByPB = 1;
 		this.timeOut_id = setInterval(function() {
-			progressBar.pureCricle(percent / 20, countByPB);
-			countByPB ++;
-		}, 100);
+			// progressBar.pureCricle(percent / num, countByPB, num);
+			// countByPB ++;
+		}, 20);
+		console.log(this.timeOut_id);
 	},
-	pureCricle: function(eqNum, countByPB){
-		if (countByPB > 20) {
+	pureCricle: function(eqNum, countByPB, num){
+		if (countByPB > num) {
 			clearInterval(this.timeOut_id);return;
 		}
+		console.log(countByPB);return;
 
 		var xy = this.annularXY(eqNum, countByPB);
 		console.log(xy);
 
-		this.ctx.fillStyle = this.barcolor;
-		console.log(this.ctx);
-		this.ctx.beginPath();
-		// this.ctx.moveTo(this.center,this.center);
-		// this.ctx.lineTo(this.center,0);
-		// this.ctx.arcTo(this.center, 0, xy.x, xy.y, this.excricle);
-		this.ctx.arcTo(5, 100, 30, 200, 10);
-		// this.ctx.lineTo(this.center,this.center);
-		this.ctx.fill();
+		this.ctx.strokeStyle = this.barcolor;
+
+		this.ctx.moveTo(this.center,this.center);
+		this.ctx.lineTo(xy.x,xy.y);
+		this.ctx.stroke();
 	},
 	annularXY: function(eqNum, countByPB){
+		console.log(countByPB);
 		var p = eqNum * countByPB;
 		var num = p / 25;
 		if (parseInt(num) === num && num != 4) {
@@ -121,20 +120,20 @@ var progressBar = {
 		var xy = {x:null,y:null};
 		switch (num) {
 			case 0://4
-				xy.x = this.center + this.excricle * Math.cos((Math.abs(90 - angle)) * this.pi);
-				xy.y = this.excricle * Math.sin(angle * this.pi);
+				xy.x = this.center + this.excricle * Math.cos(Math.abs(90 - angle) * this.pi);
+				xy.y = this.center - this.excricle * Math.sin(Math.abs(90 - angle) * this.pi);
 				break;
 			case 1://1
 				xy.x = this.center + this.excricle * Math.cos((angle - 90) * this.pi);
-				xy.y = this.center - this.excricle * Math.sin(angle * this.pi) + this.center;
+				xy.y = this.center + this.excricle * Math.sin((angle - 90) * this.pi);
 				break;
 			case 2://2
 				xy.x = this.center + this.excricle * Math.cos((angle - 90) * this.pi);
-				xy.y = this.center * 2 + this.excricle * Math.sin(angle * this.pi);
+				xy.y = this.center + this.excricle * Math.sin((angle - 90) * this.pi);
 				break;
 			case 3://3
-				xy.x = this.excricle * Math.cos(angle * this.pi);
-				xy.y = Math.abs(this.excricle * Math.sin(angle * this.pi));
+				xy.x = this.center + this.excricle * Math.cos((angle - 90) * this.pi);
+				xy.y = this.center + this.excricle * Math.sin((angle - 90) * this.pi);
 				break;
 			case 4://终点
 				xy.x = this.center;
@@ -145,8 +144,15 @@ var progressBar = {
 	}
 }
 
-progressBar.init('pureColorAnnular', 30, 100, 'rgb(255, 255, 255)', '#3366FF', 'progressBar');
+progressBar.init('pureColorAnnular', 60, 100, 'rgb(255, 255, 255)', '#3366FF', 'progressBar', 100);
 
 window.onresize = function(){
-	progressBar.init('pureColorAnnular', 30, 100, 'rgb(255, 255, 255)', '#3366FF', 'progressBar');
+	if (resize_num !== 0) {
+		return;
+	}
+	resize_num ++;
+	progressBar.init('pureColorAnnular', 60, 100, 'rgb(255, 255, 255)', '#3366FF', 'progressBar', 100);
+	setTimeout(function(){
+		resize_num = 0;
+	}, 100);
 }
